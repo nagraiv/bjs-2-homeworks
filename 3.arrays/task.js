2330d971ed02c6eef40c92c665477bd7c287b34b
GraN <117268180+nagraiv@users.noreply.github.com>
2023-06-11 21:09:36 +0700

    third task is done

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,17 @@
 function compareArrays(arr1, arr2) {
-  
+  if (!(arr1 instanceof Array) || !(arr2 instanceof Array) || arr1.length !== arr2.length) {
+      return false;
+  }
+  return arr1.every((current, index) => current === arr2[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    if (!(users instanceof Array) || users.length === 0) {
+        return 0;
+    }
+    return users.filter(item => item.gender === gender)
+        .map(item => item.age)
+        .reduce((accumulator, item, index, array) => {
+            return index === array.length - 1 ? (accumulator + item) / array.length : accumulator + item;
+        }, 0);
 }
