bd5646b0e689fa940861a8b3511d96c83d0558c4
GraN <117268180+nagraiv@users.noreply.github.com>
2023-06-13 19:48:14 +0700

    4th task is done

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,29 @@
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function (...marksToAdd) {
+    if (this.hasOwnProperty('marks')) {
+        this.marks.push(...marksToAdd);
+    }
 }
 
 Student.prototype.getAverage = function () {
-  
+    return this.hasOwnProperty('marks') && this.marks.length ?
+        this.marks.reduce((acc, el, ind, array) => {
+            return ind === array.length - 1 ? (acc + el) / array.length : acc + el;
+        }, 0) : 0;
 }
 
 Student.prototype.exclude = function (reason) {
-  
+    delete this.marks;
+    delete this.subject;
+    this.excluded = reason;
 }
