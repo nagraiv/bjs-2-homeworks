6bdb79e7a580a07db7de4c4fd1098d6c372072b0
GraN <117268180+nagraiv@users.noreply.github.com>
2023-06-18 01:56:46 +0700

    6th task is done

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,50 @@
-﻿
+﻿function parseCount(value) {
+    const result = Number.parseFloat(value);
+    if (Number.isNaN(result)) {
+        throw new Error("Невалидное значение");
+    }
+    return result;
+}
+
+function validateCount(goodsQuantity) {
+    try {
+        return parseCount(goodsQuantity);
+    } catch (error) {
+        return error;
+    }
+}
+
+class Triangle {
+    constructor(a, b, c) {
+        this.sides = [a, b, c].sort((x, y) => x - y);
+        if (this.sides[0] + this.sides[1] < this.sides[2]) {
+            throw new Error('Треугольник с такими сторонами не существует');
+        }
+    }
+
+    get perimeter() {
+        return this.sides.reduce((acc, el) => acc + el, 0);
+    }
+
+    get area() {
+        const p = this.perimeter / 2;
+        const s = Math.sqrt(p * (p - this.sides[0]) * (p - this.sides[1]) * (p - this.sides[2]));
+        return +s.toFixed(3);
+    }
+}
+
+function getTriangle(a, b, c) {
+    try {
+        return new Triangle(a, b, c);
+    } catch (e) {
+        return {
+            get perimeter() {
+                return 'Ошибка! Треугольник не существует';
+            },
+            get area() {
+                return 'Ошибка! Треугольник не существует';
+            },
+        };
+    }
+}
+
